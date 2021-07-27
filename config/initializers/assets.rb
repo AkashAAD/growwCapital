ba5443c38960dfee559d42501ef1e05262139084
Akash D <deshbhratarakash@gmail.com>
2021-07-26 22:41:01 -0700

    (Fixed) ui issues and assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,4 +3,6 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 File.join(".", "**/*.rb")
 Rails.application.config.assets.paths += Dir["#{Rails.root}/vendor/assets/*"].sort_by { |dir| -dir.size }
 Rails.application.config.assets.precompile += Dir["#{Rails.root}/app/assets/stylesheets/*"].sort_by { |dir| -dir.size }
+Rails.application.config.assets.precompile += Dir["#{Rails.root}/app/assets/stylesheets/users/*"].sort_by { |dir| -dir.size }
 Rails.application.config.assets.precompile += Dir["#{Rails.root}/app/assets/javascripts/*"].sort_by { |dir| -dir.size }
+Rails.application.config.assets.precompile += Dir["#{Rails.root}/app/assets/javascripts/users/*"].sort_by { |dir| -dir.size }
