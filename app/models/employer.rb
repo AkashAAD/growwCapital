3979e759f0483a064ab3943ef2b26e708d43b714
Akash D <deshbhratarakash@gmail.com>
2021-08-13 20:52:11 -0700

    (Employeer list)

--- app/models/employer.rb
+++ app/models/employer.rb
@@ -1,3 +1,19 @@
 class Employer < ApplicationRecord
+  validates_uniqueness_of :name
   scope :search_employer, ->(text) { where("name LIKE ?", "%#{text}%") }
+
+  def self.add_employers
+    require 'csv'
+    file = File.join(Rails.root, 'CompanyList.csv')
+    file = File.read(file)
+    csv = CSV.parse(file, :headers => true)
+    employer = self.new(name: 'AMITY University')
+    employer.save
+    csv.each do |row|
+      employer = self.new(name: row[1])
+      employer.save
+      puts row[1]
+    end
+  end
+
 end
