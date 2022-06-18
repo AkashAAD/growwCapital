d965e435d799fc8b0220fec81310755b6bdf9935
AkashAAD <deshbhratarakash@gmail.com>
2022-06-18 10:33:22 +0530

    updated the names

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   include SlugConcern
 
-  has_many :channel_partner_products
+  has_many :channel_partner_products, dependent: :destroy
   has_many :channel_partners, through: :channel_partner_products
 
   validates :name, presence: true, uniqueness: true
